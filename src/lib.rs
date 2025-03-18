df27fee6c319fc6958d7c7e473d8f57981c56e60
erhant <erhany96@gmail.com>
2025-03-19 00:35:44 +0300

    some refactors and formattings

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,9 @@ pub use cache::{CacheManager, ZKeyCache};
 use file_wrapper::FileWrapper;
 use icicle_bn254::curve::{CurveCfg, G2CurveCfg, ScalarField};
 use icicle_core::curve::{Affine, Projective};
+use icicle_runtime::eIcicleError;
 use proof_helper::groth16_prove_helper;
-use std::time::Instant;
+use std::path::Path;
 
 pub type F = ScalarField;
 pub type C1 = CurveCfg;
@@ -20,40 +21,44 @@ pub type G2 = Affine<C2>;
 pub type ProjectiveG1 = Projective<C1>;
 pub type ProjectiveG2 = Projective<C2>;
 
-fn try_load_and_set_backend_device(device_type: &str) {
+/// Attempts to load and set a backend device.
+///
+/// Possible device names:
+/// - `CPU`
+/// - `CUDA`
+/// - `METAL`
+fn try_load_and_set_backend_device(device_type: &str) -> Result<(), eIcicleError> {
     if device_type != "CPU" {
-        icicle_runtime::runtime::load_backend_from_env_or_default().unwrap();
+        icicle_runtime::runtime::load_backend_from_env_or_default()?;
     }
     let device = icicle_runtime::Device::new(device_type, 0 /* =device_id*/);
-    icicle_runtime::set_device(&device).unwrap();
+    icicle_runtime::set_device(&device)
 }
 
 pub fn groth16_prove(
-    witness: &str,
-    zkey: &str,
-    proof: &str,
-    public: &str,
+    witness_path: impl AsRef<Path>,
+    zkey_path: impl AsRef<Path>,
+    proof: impl AsRef<Path>,
+    public: impl AsRef<Path>,
     device: &str,
     cache_manager: &mut CacheManager,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let start = Instant::now();
-    try_load_and_set_backend_device(device);
-
-    let cache_key = format!("{}_{}", zkey, device);
-    
-    if !cache_manager.contains(&cache_key) {
-        let computed_cache = cache_manager.compute(zkey)?;
-        cache_manager.insert_cache(&cache_key, computed_cache);
+    if let Err(e) = try_load_and_set_backend_device(device) {
+        eprintln!("could not load and set backend device: {:?}", e);
     }
 
+    // load from cache w.r.t zkey and device
+    let cache_key = format!("{}_{}", zkey_path.as_ref().display(), device);
+    if !cache_manager.contains(&cache_key) {
+        let computed_cache = cache_manager.compute(zkey_path)?;
+        cache_manager.insert_cache(&cache_key, computed_cache);
+    }
     let zkey_cache = cache_manager.get_cache(&cache_key);
 
-    let (proof_data, public_signals) = groth16_prove_helper(witness, zkey_cache)?;
-
+    // save to file (TODO: can be returned instead to be saved elsewhere)
+    let (proof_data, public_signals) = groth16_prove_helper(witness_path, zkey_cache)?;
     FileWrapper::save_json_file(proof, &proof_data)?;
     FileWrapper::save_json_file(public, &public_signals)?;
-
-    println!("proof took: {:?}", start.elapsed());
 
     Ok(())
 }
